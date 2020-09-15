e56ec2462ff8f06e4b0d44c0512cd6ba4358b973
CWD <cwd@cloudspokes.com>
2020-09-15 23:17:16 +0000

    reading from source json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const fs = require('fs').promises
 const config = require('config')
 const submissionApi = require('@topcoder-platform/topcoder-submission-api-wrapper')
 
@@ -12,25 +13,36 @@ async function createSubs() {
   const type = 'Contest Submission'
   const submissions = new Array()
 
-  submissions[submissions.length] = { url: 'https://tc-test-submission-scan.s3.amazonaws.com/good.zip', memberId: 23225544, challengeId: 30056738}
+  try {
+    const challengesFile = await fs.readFile(config.FILE_CHALLENGES)
+    const data = JSON.parse(challengesFile.toString())
+console.log(JSON.stringify(data.challenges[0],null," "))
+/*
+    _.forEach(data.challenges, async (c) => {
+      //{ url: 'https://tc-test-submission-scan.s3.amazonaws.com/good.zip', memberId: 23225544, challengeId: 30056738 }
 
-  _.forEach(submissions, async (s) => {
-    const url = s.url
-
-    const sub = {
-      url,
-      type,
-      memberId: s.memberId,
-      challengeId: s.challengeId
-    }
-    try {
-      const rec = await submissionApiM2MClient.createSubmission(sub)
-      console.log(rec.body)
-    } catch (e) {
-      console.log(e)
-    }
-    
+      _.forEach(c.submissions, async (s) => {
+        const url = s.url
+        const sub = {
+          url,
+          type,
+          memberId: s.memberId,
+          challengeId: s.challengeId
+        }
+        try {
+          const rec = await submissionApiM2MClient.createSubmission(sub)
+          console.log(rec.body)
+        } catch (e) {
+          console.log(e)
+        }
+        })
   })
+*/
+  } catch(e) {
+    console.log(e)
+  }
+
+
 }
 
 createSubs()
